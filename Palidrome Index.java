4ce77ccdb2f4c04f233329ddf22a3c67d19adb51
Satyanarayana Madasu <127476823+MSatya1431@users.noreply.github.com>
2023-03-24 10:42:27 +0530

    View File

---
+++ Palidrome Index.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) 
+    {
+    // Write your code here
+        int n=s.length();
+        for(int i=0, j=n-1;i<j;i++, j--)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                if(isPalindrome(s, i+1, j))
+                {
+                    return i;
+                }
+                if(isPalindrome(s, i, j-1))
+                {
+                    return j;
+                }
+                return -1;
+            }
+        }
+        return -1;
+    }
+    private static boolean isPalindrome(String s, int i, int j)
+    {
+        while(i<j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.palindromeIndex(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
